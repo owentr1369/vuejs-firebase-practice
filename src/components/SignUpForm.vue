dddd2d2bf02b4e5ca6b067e038bb0aa12059e5b4
TranTam <tamvaa1306@gmail.com>
2023-12-25 17:57:37 +0700

    Add SignUpForm component and update App.vue

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.self.prevent="onSignUp">
+    <input type="text" v-model="signUpForm.email" placeholder="username" />
+    <input type="email" v-model="signUpForm.username" placeholder="username" />
+    <input
+      type="password"
+      v-model="signUpForm.password"
+      placeholder="password"
+    />
+    <input type="submit" title="Sign Up" />
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const signUpForm = ref({ username: "", password: "", email: "" });
+
+const onSignUp = () => {
+  console.log({
+    username: signUpForm.value.username,
+    password: signUpForm.value.password,
+    email: signUpForm.value.email,
+  });
+};
+</script>
+
+<style>
+</style>
